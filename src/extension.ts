f962da7d2132f740fecd9e186dd1cb7a93269ca8
LaurentTreguier <laurent@treguier.org>
2016-12-02 21:38:17 +0100

    Start using meta-pkg

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,37 @@
 'use strict';
 
 import * as vsc from 'vscode';
-import {MODES} from './modes';
+import * as pkg from 'meta-pkg';
+import { MODES } from './modes';
 import Formatter from './formatter';
 
 export function activate(context: vsc.ExtensionContext) {
+    let choices: string[] = [];
+    let installerChoices = {};
+    let uncrustify = {
+        targets: ['uncrustify'],
+        backends: {
+            packagekit: ['uncrustify'],
+            brew: 'uncrustify'
+        }
+    };
+
+    pkg.isInstalled(uncrustify)
+        .then((installed) => !installed && pkg.getInstallers(uncrustify)
+            .then((installers) => {
+                if (installers.length) {
+                    installers.forEach((installer) => {
+                        let choice = 'Install using ' + installer.name;
+                        choices.push(choice);
+                        installerChoices[choice] = installer;
+                    });
+
+                    return vsc.window.showWarningMessage('Uncrustify does not seem to be installed', ...choices);
+                }
+            }))
+        .then((choice) => choice && installerChoices[choice].install())
+        .then((result) => result && vsc.window.showInformationMessage('Uncrustify is now installed'));
+
     let subscribtion = vsc.languages.registerDocumentFormattingEditProvider(MODES, new Formatter());
     context.subscriptions.push(subscribtion);
 };
