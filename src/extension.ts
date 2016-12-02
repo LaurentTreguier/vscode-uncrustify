596362e4e92f045a89ddaea107aed62c29a34b35
LaurentTreguier <laurent@treguier.org>
2016-12-02 21:48:08 +0100

    Add 'installing...' message

--- src/extension.ts
+++ src/extension.ts
@@ -8,6 +8,7 @@ import Formatter from './formatter';
 export function activate(context: vsc.ExtensionContext) {
     let choices: string[] = [];
     let installerChoices = {};
+    let message: vsc.Disposable;
     let uncrustify = {
         targets: ['uncrustify'],
         backends: {
@@ -29,8 +30,18 @@ export function activate(context: vsc.ExtensionContext) {
                     return vsc.window.showWarningMessage('Uncrustify does not seem to be installed', ...choices);
                 }
             }))
-        .then((choice) => choice && installerChoices[choice].install())
-        .then((result) => result && vsc.window.showInformationMessage('Uncrustify is now installed'));
+        .then((choice) => {
+            if (choice) {
+                message = vsc.window.setStatusBarMessage('Installing uncrustify...');
+                return installerChoices[choice].install();
+            }
+        })
+        .then((result) => {
+            if (result) {
+                message.dispose();
+                vsc.window.showInformationMessage('Uncrustify is now installed');
+            }
+        });
 
     let subscribtion = vsc.languages.registerDocumentFormattingEditProvider(MODES, new Formatter());
     context.subscriptions.push(subscribtion);
